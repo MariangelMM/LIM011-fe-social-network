b7e0a3b73cbfc647af418e033f67fd3d89d8dd4d
Karen753 <sulca753@gmail.com>
2020-01-20 12:46:39 -0500

    avance de test  firestore

--- test/mockfirestore.spec.js
+++ test/mockfirestore.spec.js
@@ -7,25 +7,42 @@ const fixtureData = {
   __collection__: {
     notes: {
       __doc__: {
-        abc123: {
-          title: 'terminar la pildora',
+        post001: {
+          post: 'probando mocks',
+          tipo: 'publico',
+          uid: 'user001',
+          name: 'karen sulca',
+          email: 'sulca753@gmail.com',
+          date: '17/01/2020',
+
 
         },
-        abc125: {
-          title: 'lavar ropa',
 
-        },
+
       },
     },
   },
 };
 
+
 global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
 
+const objectpost = {
+  post: 'probando mocks',
 
+};
+const tipopost = {
+  tipo: 'publico',
+};
+const datausuario = {
+
+  uid: 'user001',
+  displayName: 'karen sulca',
+  email: 'sulca753@gmail.com',
+
+};
 describe('postUser', () => {
-  it('Debería poder agregar una nota', () => postUser('preparar la pildora').then((data) => {
-    console.log(data);
+  it('Debería poder agregar una nota', () => postUser(objectpost, tipopost, datausuario).then((data) => {
     expect(data).toBe('la nota fue agregada');
   }));
 });
